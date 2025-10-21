4d9e3839451c628bfa972dd512263002706dccdf
Chris Whaley <whaleychrisp@gmail.com>
2025-10-21 11:01:52 -0400

    doc: Documentation for updated and new functions

--- tests/testthat/test-league.R
+++ tests/testthat/test-league.R
@@ -1,9 +1,3 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
-
-
-
-test_that("multiplication works", {
-  expect_type(yf_league_settings(league_key, "roster"), "dataframe")
-})
